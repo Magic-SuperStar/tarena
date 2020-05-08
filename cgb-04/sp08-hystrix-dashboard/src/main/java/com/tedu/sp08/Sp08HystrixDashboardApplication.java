bbf146f0736acb701bfe7d7639206841c4af6883
ljz <8589273+Magic-SuperStar@users.noreply.github.com>
2020-05-08 12:37:56 +0800

    hystrix-dashboard:仪表盘 @EnableEurekaClient->@EnableDiscoveryClient

--- cgb-04/sp08-hystrix-dashboard/src/main/java/com/tedu/sp08/Sp08HystrixDashboardApplication.java
+++ cgb-04/sp08-hystrix-dashboard/src/main/java/com/tedu/sp08/Sp08HystrixDashboardApplication.java
@@ -2,10 +2,10 @@ package com.tedu.sp08;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableHystrixDashboard
 @SpringBootApplication
 public class Sp08HystrixDashboardApplication {
